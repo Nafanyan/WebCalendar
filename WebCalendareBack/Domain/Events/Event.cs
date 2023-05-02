601a5ddbc4590885838315c2d879c0d8d35d5407
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2023-05-02 16:06:33 +0300

    create Domain Layer

---
+++ WebCalendareBack/Domain/Events/Event.cs
@@ -0,0 +1,33 @@
+﻿namespace WebCalendar.Domain.Events
+{
+    public class Event
+    {
+        public long Id { get; private set; }
+        public string Record { get; private set; }
+        public string? Description { get; private set; }
+        public DateTime StartEvent { get; private set; }
+        public DateTime EndEvent { get; private set; }
+
+        public Event( string record, string description, DateTime startEvent, DateTime endEvent)
+        {
+            Record = record;
+            Description = description;
+            StartEvent = startEvent;
+            EndEvent = endEvent;
+        }
+        public void UpdateRecord(string record)
+        {
+            Record = record;
+        }
+        public void UpdateDescription(string description)
+        {
+            Description = description;
+        }
+        public void UpdateDateEvent(DateTime startEvent, DateTime endEvent)
+        {
+            StartEvent = startEvent;
+            EndEvent = endEvent;
+        }
+
+    }
+}

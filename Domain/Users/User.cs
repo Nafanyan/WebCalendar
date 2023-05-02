1beb6accff3ee0a6a99a70509eeedfc9afa5229b
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2023-05-02 16:39:55 +0300

    create Domain layer

---
+++ Domain/Users/User.cs
@@ -0,0 +1,40 @@
+﻿using WebCalendar.Domain.Events;
+
+namespace WebCalendar.Domain.Users
+{
+    public class User
+    {
+        public long Id { get; init; }
+        public List<Event> Events { get; private set; }
+        public string Login { get; private set; }
+        private string PasswordHash { get;  set; }
+
+        public User(string login, string passwordHash)
+        {
+            Events = new List<Event>();
+            Login = login;
+            PasswordHash = passwordHash;
+        }
+
+        public void UpdateLogin(string login, string passwordHash)
+        {
+            if (CheckPasswordHash(passwordHash))
+            {
+                Login = login;
+            }
+        }
+        public bool UpdatePasswordHash(string oldPasswordHash, string newPasswordHash)
+        {
+            if (CheckPasswordHash(oldPasswordHash))
+            {
+                PasswordHash = newPasswordHash;
+                return true;
+            }
+
+            return false;
+        }
+        public bool CheckPasswordHash(string passwordHash) => PasswordHash == passwordHash;
+
+
+    }
+}

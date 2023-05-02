639df4130c27142729977304b91e660f00323d5d
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2023-05-02 16:00:01 +0300

    refresh

--- WebCalendareBack/WebCalendar.Application/Users/UserUpdating/UpdateUserCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebCalendar.Application.Users.UserUpdating
-{
-    public class UpdateUserCommand
-    {
-        public long Id { get; init; }
-        public string Login { get; init; }
-        public string PasswordHash { get; init; }
-    }
-}

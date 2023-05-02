bc53385cb46145f3b2e6eb1b2f0556f63c1faeb0
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2023-05-02 16:44:38 +0300

    create Domain layer

---
+++ Domain/Events/IEventRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace WebCalendar.Domain.Events
+{
+    public interface IEventRepository
+    {
+        Event GetById (long id);
+        void Add (Event e);
+        void Update (Event e);
+        void Delete (long id);
+    }
+}
---
+++ Domain/Validation/EventValidation/ValidationEvent.cs
@@ -0,0 +1,50 @@
+﻿using WebCalendar.Domain.Events;
+
+namespace WebCalendar.Domain.Validation.EventValidation
+{
+    public class ValidationEvent
+    {
+        private IEventRepository _eventRepository;
+        public ValidationEvent(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        public void CheckingContentInRepository(long id)
+        {
+            if (_eventRepository.GetById(id) == null)
+            {
+                throw new EventException("There is no record with this id");
+            }
+        }
+
+        public void CheckingTheDate(DateTime startEvent, DateTime endEvent)
+        {
+            if (startEvent > endEvent)
+            {
+                throw new EventException("The start date cannot be later than the end date");
+            }
+        }
+
+        public void CheckingTheRecord(string record)
+        {
+            if (record == null)
+            {
+                throw new EventException("The record cannot be empty/cannot be null");
+            }
+        }
+
+        public void CheckingDateForNull(DateTime startEvent, DateTime endEvent)
+        {
+            if (startEvent == null)
+            {
+                throw new EventException("The start date cannot be empty/cannot be null");
+            }
+            
+            if (endEvent == null)
+            {
+                throw new EventException("The end date cannot be empty/cannot be null");
+            }
+        }
+    }
+}

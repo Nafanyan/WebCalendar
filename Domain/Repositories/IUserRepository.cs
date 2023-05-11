6c04360e962644ec8e5d19a7fb2acf0102838396
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2023-05-11 14:56:31 +0300

    Правки с названиями

--- Domain/Repositories/IUserRepository.cs
+++ Domain/Repositories/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace Domain.Repositories
 {
     public interface IUserRepository : IAddedRepository<User>, IRemovableRepository<User>
     {
-        Task<User> GetUser(long id);
-        Task<IReadOnlyList<Event>> GetEvents(User user);
+        Task<User> GetById(long id);
+        Task<IReadOnlyList<Event>> GetEvents(long userId);
     }
 }

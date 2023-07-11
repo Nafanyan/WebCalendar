c27514321aa03ec48e9b488a38e0b2d9e316dc8a
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2023-07-11 08:30:44 +0300

    Убрал в классе Access токен так как его необходимо не сравнивать с имеющимся значением, а декодировать

--- Domain/Entities/UserAuthorizationToken.cs
+++ Domain/Entities/UserAuthorizationToken.cs
@@ -3,21 +3,15 @@ namespace Domain.Entities
 {
     public class UserAuthorizationToken
     {
-        public string AccessToken { get; private set; }
         public string RefreshToken { get; private set; }
         public long UserId { get; init; }
 
-        public UserAuthorizationToken (string accessToken, string refreshToken, long userId)
+        public UserAuthorizationToken (string refreshToken, long userId)
         {
-            AccessToken = accessToken;
             RefreshToken = refreshToken;
             UserId = userId;
         }
 
-        public void SetAccessToken(string accessToken)
-        {
-            AccessToken = accessToken;
-        }
         public void SetRefreshToken(string refreshToken)
         {
             RefreshToken = refreshToken;

7cc75c9e0e6d5777b93ff379604474c33ce47c0e
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2023-07-13 09:46:26 +0300

    Начал делать аттрибут для валидации access токена

--- Web/Presentation.Intranet.Api/TokenValidationAttribute.cs
+++ Web/Presentation.Intranet.Api/TokenValidationAttribute.cs
@@ -1,17 +1,9 @@
-﻿using Domain.Entities;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Text;
-
+﻿
 namespace Presentation.Intranet.Api
 {
     public class TokenValidationAttribute : Attribute
     {
         public long UserId { get; }
         public bool IsValid { get; }
-
     }
 }
